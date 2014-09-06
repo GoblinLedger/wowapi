4bfbadfd43ac800775d0a19c47664aaf6499108e
Billy Overton <overton.billy@gmail.com>
2014-09-06 01:44:10 -0400

    Added a utility module for dealing with wowapi data.

---
+++ wowapi/utility.py
@@ -0,0 +1,13 @@
+def format_currency(copper, format='{gold}g {silver}s {copper}c'):
+    """Takes a copper amount and formats it into a pretty string
+
+    Given a integer copper amount, caculates the represented amount of gold,
+    silver, and excess copper and returns a representational string based on a
+    provided format. Defaults to {gold}g {silver}s {copper}c.
+    """
+
+    gold = copper // 10000
+    copper = copper % 10000
+    silver = copper // 100
+    copper = copper % 100
+    return format.format(gold=gold, silver=silver, copper=copper)
