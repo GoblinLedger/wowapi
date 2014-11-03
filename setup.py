1d4565fabb4f512c909289e7ec638dd400be3384
Billy Overton <overton.billy@gmail.com>
2014-11-02 23:24:24 -0500

    Bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wowapi',
-    version='0.1.0',
+    version='0.2.0',
     packages=['wowapi'],
     install_requires=[
         'requests',
