e450b4dd0a096bd359c0bea86c80c464eb2ed571
Billy Overton <overton.billy@gmail.com>
2016-03-02 20:18:27 -0500

    Adds tests for new api functions

--- tests/test_wowapi_utility.py
+++ tests/test_wowapi_utility.py
@@ -6,3 +6,18 @@ def test_currency_conversion():
     assert gold == 754
     assert silver == 9
     assert copper == 84
+
+def test_format_character_faction():
+    for faction in wowapi.utility.FACTIONS:
+        assert wowapi.utility.format_character_faction(faction) == wowapi.utility.FACTIONS[faction]
+
+    with pytest.raises(ValueError):
+        wowapi.utility.format_character_faction("unknownFaction")
+
+
+def test_format_character_gender():
+    for gender in wowapi.utility.GENDERS:
+        assert wowapi.utility.format_character_gender(gender) == wowapi.utility.GENDERS[gender]
+
+    with pytest.raises(ValueError):
+        wowapi.utility.format_character_gender("unknownGender")
